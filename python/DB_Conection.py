1d5ce16116237d6277076288ccadca20a61ab359
Fernando Ortiz Rico Celio <fernando_oc96@hotmail.com>
2017-04-15 01:20:30 -0500

    Se logra conexión con la bd ‘snapchat’; Se logran INSERTS para crear nuevos usuarios; Se reestructura el archivo DB_Connection, falta modificar métodos.

--- python/DB_Conection.py
+++ python/DB_Conection.py
@@ -1,38 +1,69 @@
-import socket
 import pymysql
-
-#Variables de DB
-db_host = 'localhost'
-db_port = 3306
-db_user = 'root'
-db_password = ''
-db_charset = 'utf8'
-db_database = 'waves'
-
-#
-conn = pymysql.connect(host=db_host, port=db_port, user=db_user, passwd=db_password, db=db_database)
-cur = conn.cursor()
-
-cur.execute("SELECT song.songID, song.songName, artist.artistName, album.albumTitle, genre.genreName FROM album, artist, genre, song WHERE song.songArtist = artist.artistID AND song.songAlbum = album.albumID AND song.songGenre = genre.genreID;")
-
-print("Existen", cur.rowcount, "artistas")
-
-for row in cur:
-    print(row)
-
-cur.close()
-conn.close()
+import socket
 
 
-# NOTAS:
-# Se cambiará la base de datos, la que se emplea es para pruebas de funcionamiento
-
-class connection:
-
+class snapDB:
+    # Get conection with database -> connection
     def getConnection(self):
-        conn = pymysql.connect(host=db_host, port=db_port, user=db_user, passwd=db_password, db=db_database)
+        # Database Variables
+        db_host = 'localhost'
+        db_port = 3306
+        db_user = 'root'
+        db_password = ''
+        db_charset = 'utf8'
+        db_database = 'snapchat'
+
+        conn = pymysql.connect(host=db_host, port=db_port, user=db_user, passwd=db_password, db=db_database, charset=db_charset)
         return conn
 
-    def addNewUser(self, name, password, ipAddress):
+
+    # Gets the device's IP Address
+    def getIPadress(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('192.0.0.8', 1027))
+        ipAddress = s.getsockname()[0]
+        return ipAddress
+
+
+    # Adds a new user to the database
+    def addUser(self, nickName, name, password):
+        # Getting connection
+        conn = self.getConnection()
         cur = conn.cursor()
-        cur.execute("INSERT into ")
+
+        cur.execute("INSERT INTO user(userNickname, userName, userPassword, userIPAddress) VALUES('"+nickName+"', '"+name+"', '"+password+"', '"+self.getIPadress()+"');")
+
+        # Changes are saved through 'commit()'
+        conn.commit()
+        cur.close()
+        conn.close()
+
+
+    # DEFS YET TO BE UPDATED!!
+
+    def getAllArtists(self):
+        conn = self.getConnection()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM artist ORDER BY artistID")
+        for row in cur:
+            print(row)
+
+        cur.close()
+        conn.close()
+
+
+
+    def getAllSongs(self):
+        conn = self.getConnection()
+        cur = conn.cursor()
+        cur.execute("SELECT song.songID, song.songName, artist.artistName, album.albumTitle, genre.genreName FROM album, artist, genre, song WHERE song.songArtist = artist.artistID AND song.songAlbum = album.albumID AND song.songGenre = genre.genreID ORDER BY song.songID;")
+
+        for row in cur:
+            print(row)
+
+        cur.close()
+        conn.close()
+
+
+a = snapDB()
+a.addUser("loco12", "Juan Gabriel", "contra123")
